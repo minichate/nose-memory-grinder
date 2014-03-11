8a8a30491afc024dd02e1a18e51c924b306999d2
Christopher Troup <ctroup@freshbooks.com>
2014-03-11 15:34:30 -0400

    Better memory size formatting and lower threshold to 5%

--- memorygrind/test_runner.py
+++ memorygrind/test_runner.py
@@ -1,30 +1,30 @@
+from __future__ import division
 from nose.plugins import Plugin
 from nose.result import TextTestResult
 import nose.core
 import gc
 import resource
 
+MEMORY_USAGE_MESSAGE = '''Memory usage too high:
+    Start:      %s
+    End:        %s
+---------------------------
+    Increase:   %s%%'''
+
 
 class MemoryTestResult(TextTestResult):
 
     def startTest(self, test):
         super(MemoryTestResult, self).startTest(test)
-        gc.collect()
         self.mem_before = self.using()
 
     def stopTest(self, test):
-        gc.collect()
         self.mem_after = self.using()
         if not self.wasSuccessful():
             pct = 100 * float(self.mem_after) / float(self.mem_before)
-            msg = '''
-Memory usage too high:
-    Start:      %s mb
-    End:        %s mb
----------------------------
-    Increase:   %s%%''' % (
-                self.mem_before / (1024 * 1024),
-                self.mem_after / (1024 * 1024),
+            msg = MEMORY_USAGE_MESSAGE % (
+                self.humanize_bytes(self.mem_before),
+                self.humanize_bytes(self.mem_after),
                 '{0:.3g}'.format(pct - 100)
             )
             self.addFailure(
@@ -37,7 +37,7 @@ Memory usage too high:
         super(MemoryTestResult, self).stopTest(test)
 
     def wasSuccessful(self):
-        if self.mem_after > self.mem_before * 1.1:
+        if self.mem_after > self.mem_before * 1.05:
             return False
         return True
 
@@ -57,9 +57,29 @@ Memory usage too high:
 
     def _get_format_string(self):
         pct = 100 * float(self.mem_after) / float(self.mem_before)
-        before = self.mem_before / (1024 * 1024)
-        after = self.mem_after / (1024 * 1024)
-        return '%smb/%smb (%s%%)' % (before, after, '{0:.3g}'.format(pct - 100))
+        before = self.mem_before
+        after = self.mem_after
+        return '%s/%s (%s%%)' % (
+            self.humanize_bytes(before),
+            self.humanize_bytes(after),
+            '{0:.3g}'.format(pct - 100)
+        )
+
+    def humanize_bytes(self, bytes, precision=1):
+        abbrevs = (
+            (1<<50L, 'PB'),
+            (1<<40L, 'TB'),
+            (1<<30L, 'GB'),
+            (1<<20L, 'MB'),
+            (1<<10L, 'kB'),
+            (1, 'bytes')
+        )
+        if bytes == 1:
+            return '1 byte'
+        for factor, suffix in abbrevs:
+            if bytes >= factor:
+                break
+        return '%.*f %s' % (precision, bytes / factor, suffix)
 
     def using(self):
         usage = resource.getrusage(resource.RUSAGE_SELF)
